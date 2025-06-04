a5d3af2603a7fe3aaa2bfb55ae24519e5d2303a3
Mohik Kler <mohikkler123@gmail.com>
2025-06-04 18:12:15 +0530

    Add simple server for Railway deployment

---
+++ simple_server.py
@@ -0,0 +1,1 @@
+from flask import Flask, send_from_directory; app = Flask(__name__); @app.route('/'); def index(): return send_from_directory('.', 'hybrid_search_frontend.html'); if __name__ == '__main__': app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
